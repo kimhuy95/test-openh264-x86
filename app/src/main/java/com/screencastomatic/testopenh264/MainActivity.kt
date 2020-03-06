4d4f546a117416de23b7826aaceeaf4aefed7ea3
huypk <huy.phan@dirox.net>
2020-03-06 12:58:26 +0700

    Initial commit

---
+++ app/src/main/java/com/screencastomatic/testopenh264/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.screencastomatic.testopenh264
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        System.loadLibrary("libopenh264")
+    }
+}
